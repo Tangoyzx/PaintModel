33b8b44f29fdc2f90338c424fa465abb66e05319
Tangoyzx <tangoyzx@gmail.com>
2015-10-29 17:42:56 +0800

    初次提交

---
+++ Assets/Scripts/DrawModelScript.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Security.Cryptography.X509Certificates;
+
+public class DrawModelScript : MonoBehaviour {
+    public Renderer renderer;
+    public Material cubeMat;
+    //懒于计算识别图的四个顶点……所以要用找几个物体来标识这个位置
+    public Transform[] marks;
+
+    public Camera camera;
+    private int frameCount;
+    private bool isFixedTexture;
+
+    
+
+    private Vector2 _webcamTex2ScreenScale;
+
+    // Use this for initialization
+    void Start() {
+        frameCount = 0;
+        isFixedTexture = false;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (!isFixedTexture) {
+            //一开始BackgroundPlane的Texture并非摄像头的图像……等一会儿再设置。
+            if (frameCount++ <= 30) return;
+            isFixedTexture = true;
+            var texture = renderer.material.mainTexture;
+            cubeMat.mainTexture = texture;
+
+            var maxScale = Mathf.Max((float)Screen.width / texture.width, (float)Screen.height / texture.height);
+
+            var t2sWidth = texture.width * maxScale;
+            var t2sHeight = texture.height * maxScale;
+
+            _webcamTex2ScreenScale.x = Screen.width / t2sWidth;
+            _webcamTex2ScreenScale.y = Screen.height / t2sHeight;
+        }
+
+
+        UpdateMatrix();
+
+    }
+
+    void UpdateMatrix() {
+        var p0 = AdjustUV(camera.WorldToViewportPoint(marks[0].position));
+        var p1 = AdjustUV(camera.WorldToViewportPoint(marks[1].position));
+        var p2 = AdjustUV(camera.WorldToViewportPoint(marks[2].position));
+        var p3 = AdjustUV(camera.WorldToViewportPoint(marks[3].position));
+
+        cubeMat.SetVector("p0", p0);
+        cubeMat.SetVector("p1", p1);
+        cubeMat.SetVector("p2", p2);
+        cubeMat.SetVector("p3", p3);
+    }
+
+    Vector3 AdjustUV(Vector3 v) {
+        var webcamX = FixWebcamTextureToScreenUV(v.x, _webcamTex2ScreenScale.x);
+        var webcamY = FixWebcamTextureToScreenUV(v.y, _webcamTex2ScreenScale.y);
+        return new Vector3(webcamX * v.z, (1 - webcamY) * v.z, v.z);
+    }
+
+    float FixWebcamTextureToScreenUV(float value, float scale) {
+        return (1.0f - scale) * 0.5f + value * scale;
+    }
+}
---
+++ Assets/Shaders/PaintingShader.shader
@@ -0,0 +1,65 @@
+﻿Shader "Unlit/PaintingShader"
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "white" {}
+	}
+	SubShader
+	{
+		Tags { "RenderType"="Opaque" }
+		LOD 100
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct v2f
+			{
+				float2 uv : TEXCOORD0;
+				float4 vertex : SV_POSITION;
+			};
+
+			sampler2D _MainTex;
+			float4 p0;
+			float4 p1;
+			float4 p2;
+			float4 p3;
+			
+			v2f vert (appdata v)
+			{
+				v2f o;
+				o.vertex = mul(UNITY_MATRIX_MVP, v.vertex);
+				o.uv = v.uv;
+				return o;
+			}
+			
+			fixed4 frag (v2f i) : SV_Target
+			{
+				float2 mid;
+				float3 newUV;
+				
+				if (i.uv.x + i.uv.y >= 1) {
+					mid = i.uv;
+					newUV = p0 * (1 - mid.x - mid.y) + p1 * mid.x + p2 * mid.y;
+				} else {
+					mid = float2(1 - i.uv.y, i.uv.x + i.uv.y - 1);
+					newUV = p2 * (1 - mid.x - mid.y) + p1 * mid.x + p3 * mid.y;
+				}
+
+				return tex2D(_MainTex, newUV.xy / newUV.zz);
+			}
+
+			ENDCG
+		}
+	}
+}
